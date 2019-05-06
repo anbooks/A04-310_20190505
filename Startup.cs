69931b27305312ba48aa2a35a1246b8a14c89703
anbooks <kevin_zhang2011@126.com>
2019-05-06 09:59:20 +0800

    其中一个暂存了

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace WebApplication8
                 options.Cookie.HttpOnly = true;
             });
             services.AddMvc();
-            var connection = @"Server=192.168.44.128;Database=NEU;Trusted_Connection=false;User Id=sa;Password=123;ConnectRetryCount=0";
+            var connection = @"Server=192.168.80.142;Database=NEU;Trusted_Connection=false;User Id=sa;Password=123;ConnectRetryCount=0";
             services.AddDbContext<NEUContext>(options => options.UseSqlServer(connection, b => b.UseRowNumberForPaging()));
         }
 
