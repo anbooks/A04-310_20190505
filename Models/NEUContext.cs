3b1f38199773ab6f1e494dbd1bc462ecfc816737
anbooks <kevin_zhang2011@126.com>
2019-05-10 09:00:51 +0800

    1111

--- Models/NEUContext.cs
+++ Models/NEUContext.cs
@@ -56,7 +56,7 @@ namespace WebApplication8.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer(@"Server=192.168.44.128;Database=NEU;User id=sa;Password=123;Trusted_Connection=false;");
+                optionsBuilder.UseSqlServer(@"Server=192.168.80.143;Database=NEU;User id=sa;Password=123;Trusted_Connection=false;");
             }
         }
 
